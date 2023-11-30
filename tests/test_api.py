b67edf1294059482fd4515ab5dfac28affac9c99
hokiejer <jeremy.rotter@gmail.com>
2023-11-30 01:48:17 +0000

    Tests fixed!!

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,19 +1,26 @@
 import unittest
 from flask_testing import TestCase
 from flask_api.api import app
+import base64
 
 class TestAPI(TestCase):
     def create_app(self):
         app.config['TESTING'] = True
         return app
 
+    def get_basic_auth_header(self, username, password):
+        credentials = base64.b64encode(f"{username}:{password}".encode()).decode('utf-8')
+        return {'Authorization': f'Basic {credentials}'}
+
     def test_get_data(self):
-        response = self.client.get('/data')
+        headers = self.get_basic_auth_header('admin', 'secret')
+        response = self.client.get('/data', headers=headers)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json, {"message": "Hello, World!"})
 
     def test_update_data(self):
-        response = self.client.post('/data', json={"message": "Hello, GitHub Copilot"})
+        headers = self.get_basic_auth_header('admin', 'secret')
+        response = self.client.post('/data', json={"message": "Hello, GitHub Copilot"}, headers=headers)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json, {"message": "Hello, GitHub Copilot"})
 
